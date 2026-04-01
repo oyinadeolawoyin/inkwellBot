0a5e0e6aa8c7da40b54bd551d955ca854086e064
oyinade olawoyin <oyinadeolawoyin@proton.me>
2026-04-01 14:05:01 +0100

    Reset dotenv to load locally and not in production

--- discordBot.js
+++ discordBot.js
@@ -1,4 +1,7 @@
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 console.log("🔥 discordBot.js is running");
 const { Client, GatewayIntentBits } = require("discord.js");
 
